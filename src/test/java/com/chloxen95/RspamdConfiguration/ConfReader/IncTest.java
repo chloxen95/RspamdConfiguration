5944a2d62a0f3e55e85788eec42d517ec6c3f6e3
chenhanlin <chenhanlin@wondersgroup.com>
2017-05-03 23:20:45 +0800

    1. Change JRE version; 2. Add file transformer test

--- src/test/java/com/chloxen95/RspamdConfiguration/ConfReader/IncTest.java
+++ src/test/java/com/chloxen95/RspamdConfiguration/ConfReader/IncTest.java
@@ -2,13 +2,67 @@ package com.chloxen95.RspamdConfiguration.ConfReader;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
+/**
+ * .inc 文件形式:
+ * 
+ * check_all_filters = false;
+ * dns {
+ *     timeout = 1s;
+ *     sockets = 16;
+ *    retransmits = 5;
+ * }
+ * classify_headers = [
+ * 		"User-Agent",
+ * 		"X-Mailer",
+ * 		"Content-Type",
+ * 		"X-MimeOLE",
+ * ];
+ * 
+ * @author chloxen95
+ *
+ */
 public class IncTest {
 
+	Map<String, Object> tranResult = new HashMap<>();
+
+	/**
+	 * 读取变量：check_all_filters = false;
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testReadVariable() {
+		String varStr = "check_all_filters = false;";
+		// long t1 = System.currentTimeMillis();
+		String[] varKV = varStr.split("=");
+		String varKey = varKV[0].trim();
+		String varValue = varKV[1].trim().endsWith(";") ? varKV[1].trim().substring(0, varKV[1].trim().length() - 1)
+				: varKV[1].trim();
+		// long t2 = System.currentTimeMillis() - t1;
+		tranResult.put(varKey, varValue);
+		System.out.println(tranResult);
+	}
+
+	/**
+	 * 读取列表：
+	 * classify_headers = [
+	 * 		"User-Agent",
+	 * 		"X-Mailer",
+	 * 		"Content-Type",
+	 * 		"X-MimeOLE",
+	 * ];
+	 */
+	@Test
+	public void testReadList() {
+
+	}
+
+	@Test
+	public void testReadMap() {
+
 	}
 
 }
--- src/test/java/com/chloxen95/RspamdConfiguration/TheFileReader/TestReadFiles.java
+++ src/test/java/com/chloxen95/RspamdConfiguration/TheFileReader/TestReadFiles.java
@@ -1,14 +1,37 @@
 package com.chloxen95.RspamdConfiguration.TheFileReader;
 
-import static org.junit.Assert.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.Test;
+import org.springframework.aop.IntroductionAwareMethodMatcher;
 
 public class TestReadFiles {
 
+	String txtName = "a.txt";
+	String incName = "options.inc";
+	String confName = "rspamd.conf";
+
 	@Test
-	public void test() {
-		
+	public void testReadFile() throws IOException {
+		// 文件路径，此处应用相对路径
+		String path = "TestFile/" + confName;
+		File file = new File(path);
+		if (!file.exists() || file.isDirectory())
+			throw new FileNotFoundException();
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String temp = null;
+		StringBuffer sb = new StringBuffer();
+		temp = br.readLine();
+		// System.out.println(temp);
+		while (temp != null) {
+			if (!temp.startsWith("#") && !"".equals(temp))
+				System.out.println(temp);
+			temp = br.readLine();
+		}
 	}
 
 }
