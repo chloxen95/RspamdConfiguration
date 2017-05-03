568dbadd1d47529ec4164a73e0140d21384c3687
chenhanlin <chenhanlin@wondersgroup.com>
2017-05-03 21:36:18 +0800

    First Commit

---
+++ src/test/java/com/chloxen95/RspamdConfiguration/ConfReader/IncTest.java
@@ -0,0 +1,14 @@
+package com.chloxen95.RspamdConfiguration.ConfReader;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IncTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/chloxen95/RspamdConfiguration/TheFileReader/TestReadFiles.java
@@ -0,0 +1,14 @@
+package com.chloxen95.RspamdConfiguration.TheFileReader;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestReadFiles {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
