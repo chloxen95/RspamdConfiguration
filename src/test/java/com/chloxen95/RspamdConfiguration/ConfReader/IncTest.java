a396341457dd650f6c37b65778ba94a11d4ef7ca
chloxen95 <chloxen95@hotmail.com>
2017-05-05 09:50:36 +0800

    add list test

--- src/test/java/com/chloxen95/RspamdConfiguration/ConfReader/IncTest.java
+++ src/test/java/com/chloxen95/RspamdConfiguration/ConfReader/IncTest.java
@@ -2,7 +2,9 @@ package com.chloxen95.RspamdConfiguration.ConfReader;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
@@ -10,17 +12,8 @@ import org.junit.Test;
 /**
  * .inc 文件形式:
  * 
- * check_all_filters = false;
- * dns {
- *     timeout = 1s;
- *     sockets = 16;
- *    retransmits = 5;
- * }
- * classify_headers = [
- * 		"User-Agent",
- * 		"X-Mailer",
- * 		"Content-Type",
- * 		"X-MimeOLE",
+ * check_all_filters = false; dns { timeout = 1s; sockets = 16; retransmits = 5;
+ * } classify_headers = [ "User-Agent", "X-Mailer", "Content-Type", "X-MimeOLE",
  * ];
  * 
  * @author chloxen95
@@ -28,7 +21,8 @@ import org.junit.Test;
  */
 public class IncTest {
 
-	Map<String, Object> tranResult = new HashMap<>();
+	Map<String, Object> tranResultMap = new HashMap<>();
+	List<Object> tranResultList = new ArrayList<>();
 
 	/**
 	 * 读取变量：check_all_filters = false;
@@ -42,22 +36,32 @@ public class IncTest {
 		String varValue = varKV[1].trim().endsWith(";") ? varKV[1].trim().substring(0, varKV[1].trim().length() - 1)
 				: varKV[1].trim();
 		// long t2 = System.currentTimeMillis() - t1;
-		tranResult.put(varKey, varValue);
-		System.out.println(tranResult);
+		tranResultMap.put(varKey, varValue);
+		System.out.println(tranResultMap);
 	}
 
 	/**
-	 * 读取列表：
-	 * classify_headers = [
-	 * 		"User-Agent",
-	 * 		"X-Mailer",
-	 * 		"Content-Type",
-	 * 		"X-MimeOLE",
-	 * ];
+	 * 读取列表： classify_headers = [ "User-Agent", "X-Mailer", "Content-Type",
+	 * "X-MimeOLE", ];
 	 */
 	@Test
 	public void testReadList() {
+		String[] varList = { "classify_headers = [", "		\"User-Agent\",", "		\"X-Mailer\",",
+				"		\"Content-Type\",", "		\"X-MimeOLE\",", "];" };
 
+		String varName = null;
+		
+		// 需要完成
+		for (String t : varList) {
+			if (t.endsWith("["))
+				varName = t.split("=")[0].trim();
+			else if(t.matches("\"([^\"]*)\""))
+				tranResultList.add(t.substring(0, t.length() - 1));
+			else if("];".equals(t))
+				break;
+		}
+		System.out.println(varName);
+		System.out.println(tranResultList);
 	}
 
 	@Test
